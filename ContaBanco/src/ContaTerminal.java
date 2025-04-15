9cabd4251e6e0737bf1c8dddf1e9130fbcb94643
MatheusFachel <matheus.fachel@gmail.com>
2025-04-15 15:17:40 -0300

    Projeto na fase desafio (Conta Terminal)

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,16 @@
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        //TODO: Conhecer e importar a Classse Scanner
+
+        //Exibir as menssagens para os usuários
+
+        //Obter pela classe Scanner os valores digitados no terminal
+
+        //Exibir a menssagem final    
+        
+        
+
+
+    }
+}
