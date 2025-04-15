2120cd76f3e4f16d99577e9b0f9d70247afa6a36
MatheusFachel <matheus.fachel@gmail.com>
2025-04-15 16:10:48 -0300

    Commit Final  do Desafio - ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,16 +1,37 @@
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        //TODO: Conhecer e importar a Classse Scanner
+        //TODO: Conhecer e importar a Classse Scanner (feito)
 
-        //Exibir as menssagens para os usuários
+        //Exibir as menssagens para os usuários (feito)
 
-        //Obter pela classe Scanner os valores digitados no terminal
+        //Obter pela classe Scanner os valores digitados no terminal (fetio)
 
-        //Exibir a menssagem final    
+        //Exibir a menssagem final (feito)   
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Para criarmos sua conta precisaremos que nos passe as seguintes infromações:");
+
+        System.err.println("Seu nome:");
+        String NomeDeUsuario = scanner.nextLine();
+
+        System.out.println("Seu número de conta");
+        int NumeroDeConta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("O número da sua agencia:");
+        String NumeroDeAgencia = scanner.nextLine();
+
+        System.out.println("Aqui digite o seu saldo de conta:");
+        Double SaldoDeConta = scanner.nextDouble();
+
+        System.out.println("CONTA CRIADA!");
+        System.out.println("Olá " + NomeDeUsuario + ",obrigado por criar uma conta em nosso banco, sua agência é " + NumeroDeAgencia + ", conta " + NumeroDeConta + " e seu saldo: " + SaldoDeConta + " já está disponível para saque.");
         
         
-
-
     }
 }
