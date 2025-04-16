cb30a92b10acf14cdc99f25b56150ea0fba5fd39
MatheusFachel <matheus.fachel@gmail.com>
2025-04-16 15:47:59 -0300

    Conclusão aula -  Condição Ternária -

--- Controle-Fluxo/src/ResultadoEscolar.java
+++ Controle-Fluxo/src/ResultadoEscolar.java
@@ -12,5 +12,14 @@ public class ResultadoEscolar {
             System.out.println("Reprovado");
         }
 
+
+        // Versão usando operador ternário
+        System.out.println("USANDO OPERADOR TERNÁRIO");
+
+
+        String Resultado = nota > 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado" ;
+
+        System.out.println(Resultado);
+
     }
 }
