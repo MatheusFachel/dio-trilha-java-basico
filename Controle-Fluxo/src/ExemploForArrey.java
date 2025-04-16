38a6ca654c8831308eb249034b076efa76a3ae8e
MatheusFachel <matheus.fachel@gmail.com>
2025-04-16 17:15:03 -0300

    Conclusão aula - For em Arrays -

---
+++ Controle-Fluxo/src/ExemploForArrey.java
@@ -0,0 +1,14 @@
+public class ExemploForArrey {
+    
+    public static void main(String[] args) {
+        
+        String alunos [] = { "FELIPE", "MATHEUS", "RENAN", "FERNANDO", "RENATA" };
+
+        for (int x = 0; x < alunos.length; x++) {
+            
+            System.out.println("O aluno no indice x = " + x + "alunos: " + alunos [x]);
+        }
+
+    }
+
+}
