70d6392e3920e41611bff4f931018923afc881d0
MatheusFachel <matheus.fachel@gmail.com>
2025-04-28 15:00:56 -0300

    Conclusão etapa - Analisando Candidatos

---
+++ Desafios/Desafio-Contador/src/ProcessoSeletivos.java
@@ -0,0 +1,22 @@
+public class ProcessoSeletivos {
+
+    public static void main(String[] args) {
+
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase==salarioPretendido) {
+            System.out.println("AGUARDANDO OS RESULTADOS DOS DEMAIS CANDIDATOS");
+        } else {
+            System.out.println("AGUARDANDO OS RESULTADOS DEMAIS RESULTADOS");
+        }   
+
+    }
+}
