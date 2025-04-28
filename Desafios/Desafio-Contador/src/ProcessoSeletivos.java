1a2e1eebd24f7398977c4130a8f83af076f7ec99
MatheusFachel <matheus.fachel@gmail.com>
2025-04-28 15:58:02 -0300

    Conclusão etapa - Selecionando candidatos

--- Desafios/Desafio-Contador/src/ProcessoSeletivos.java
+++ Desafios/Desafio-Contador/src/ProcessoSeletivos.java
@@ -1,3 +1,6 @@
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivos {
 
     public static void main(String[] args) {
@@ -5,6 +8,27 @@ public class ProcessoSeletivos {
         analisarCandidato(1900.0);
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos () {
+
+        String [] candidatos = {"FELIPE", "MARCIA", "RENATA", "LUCAS", "MATHEUS", "NICOLE" };
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;         
+        while ( candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];     
+            double salarioPretendido = valorPretendido();
+            
+            System.out.println("O candidato " + candidato + " Solicitou este valor de salário " + salarioPretendido);
+            if (salarioBase >= salarioPretendido ) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            } 
+            candidatosAtual++;
+        }
 
     }
 
@@ -18,5 +42,11 @@ public class ProcessoSeletivos {
             System.out.println("AGUARDANDO OS RESULTADOS DEMAIS RESULTADOS");
         }   
 
+    }
+
+    private static double valorPretendido() {
+    
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    
     }
 }
